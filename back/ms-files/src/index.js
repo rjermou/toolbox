cb3faa5e09ee3eba45d7de67a478b7e02cc74a64
Roberto Jermou <rjermou@munidigital.com>
2023-05-16 23:48:35 -0300

    I've added documentation

--- back/ms-files/src/index.js
+++ back/ms-files/src/index.js
@@ -1,16 +1,19 @@
 const express = require('express')
 const properties = require('getconfig')
-const cors = require('cors');
+const cors = require('cors')
 const expressPort = properties.express.port || 3031
 const expressHost = properties.express.host || '0.0.0.0'
+const swaggerUi = require('swagger-ui-express')
+const swaggerDocument = require('../docs/swagger.json')
 
 const app = express()
 
 // Configuration
-app.use(cors(properties.cors));
+app.use(cors(properties.cors))
 app.use(express.json())
 
 // Routes
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 app.use('/', require('./application/routes/index'))
 
 app.listen(expressPort, expressHost, () => {
